784246fee030331a061b82cefec2e97809b1a824
Evbuomwan Abigel Osaretin <naotaeki@gmail.com>
2024-12-02 15:47:44 +0100

    added new function

--- basics/script.js
+++ basics/script.js
@@ -2,3 +2,9 @@ function Player(nick){
     this.nickname = nick;
     this.playedMatch = 0;
 }
+
+Player.prototype.play = function(){
+    this.playedMatch += 1;
+    console.log(this.nickname + " played " + this.playedMatch + " matches")
+}
+
