8ebf4e15327052a944c899d4098c4324e730d034
Evbuomwan Abigel Osaretin <evbuomwan.abigel@BOLYAI.LOCAL>
2025-01-17 10:07:03 +0100

    megoldás

--- arraylist/main.js
+++ arraylist/main.js
@@ -15,8 +15,20 @@ class ArrayList{ //PascalCase, snake_case, Cebab_case, camelCase
     }
 
     AddElement(item){
-        this.#state[this.#count++] = item; //kulcs és érték
-        console.log(this.#state)
+        const index = this.#count;
+        this.#state[index] = item; //kulcs és érték
+        Object.defineProperty(this, index, { 
+            get:function()
+            { 
+                return this.#state[index]
+            }, 
+            set:function(value)
+            { 
+                this.#state[index] = value;
+            } 
+        })
+
+        this.#count++;
     }
 
     Clear(){
@@ -32,10 +44,11 @@ osztaly.AddElement('alma');
 osztaly.AddElement('barack');
 osztaly.AddElement({nev:'szőlő'});
 console.log(osztaly[0]);
+console.log(osztaly[2]);
 
 osztaly.Clear();
 
-const alma = {}
-Object.defineProperty(alma, 'nev', { value: "Ferenc", writable: true})  //alma objektumnál, a ferenc érték kulcsa a név lesz
-alma.nev = 'asd'
-console.log(alma);
+// const alma = {}
+// Object.defineProperty(alma, 'nev', { value: "Ferenc", writable: true})  //alma objektumnál, a ferenc érték kulcsa a név lesz
+// alma.nev = 'asd'
+// console.log(alma);
